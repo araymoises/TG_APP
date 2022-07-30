1354f6d8ff99635fd8d5e35eed1a41c18ee74e6b
Moises Aray <araymoises55@gmail.com>
2022-07-29 22:20:04 -0400

    Setting Viro library.

--- src/private/components/Home.js
+++ src/private/components/Home.js
@@ -1,16 +1,55 @@
-import React from 'react';
-import { createDrawerNavigator } from '@react-navigation/drawer';
+import React, {useState} from 'react';
+import {StyleSheet} from 'react-native';
 import {
-  Text,
-  View,
-} from 'native-base';
+  ViroARScene,
+  ViroText,
+  ViroTrackingStateConstants,
+  ViroARSceneNavigator,
+} from '@viro-community/react-viro';
 
-const Home = () => {
+const HelloWorldSceneAR = () => {
+  const [text, setText] = useState('Initializing AR...');
+
+  function onInitialized(state, reason) {
+    console.log('guncelleme', state, reason);
+    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
+      setText('Crisleivys becerra');
+    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
+      // Handle loss of tracking
+    }
+  }
+
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-      <Text>Home Screen Private</Text>
-    </View>
+    <ViroARScene onTrackingUpdated={onInitialized}>
+      <ViroText
+        text={text}
+        scale={[0.5, 0.5, 0.5]}
+        position={[0, 0, -1]}
+        style={styles.helloWorldTextStyle}
+      />
+    </ViroARScene>
   );
 };
 
-export default Home;
+export default () => {
+  return (
+    <ViroARSceneNavigator
+      autofocus={true}
+      initialScene={{
+        scene: HelloWorldSceneAR,
+      }}
+      style={styles.f1}
+    />
+  );
+};
+
+var styles = StyleSheet.create({
+  f1: {flex: 1},
+  helloWorldTextStyle: {
+    fontFamily: 'Arial',
+    fontSize: 30,
+    color: '#ffffff',
+    textAlignVertical: 'center',
+    textAlign: 'center',
+  },
+});
