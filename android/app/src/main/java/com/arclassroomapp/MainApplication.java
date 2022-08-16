02668fc972ff9bec351123af3c8b6d914d14a205
Moises Aray <araymoises55@gmail.com>
2022-08-16 16:17:54 -0400

    Revert "Setting Viro library."
    This reverts commit 1354f6d8ff99635fd8d5e35eed1a41c18ee74e6b.

--- android/app/src/main/java/com/arclassroomapp/MainApplication.java
+++ android/app/src/main/java/com/arclassroomapp/MainApplication.java
@@ -10,7 +10,6 @@ import com.facebook.react.ReactPackage;
 import com.facebook.soloader.SoLoader;
 import java.lang.reflect.InvocationTargetException;
 import java.util.List;
-import com.viromedia.bridge.ReactViroPackage;
 
 public class MainApplication extends Application implements ReactApplication {
 
@@ -27,8 +26,6 @@ public class MainApplication extends Application implements ReactApplication {
           List<ReactPackage> packages = new PackageList(this).getPackages();
           // Packages that cannot be autolinked yet can be added manually here, for example:
           // packages.add(new MyReactNativePackage());
-          packages.add(new ReactViroPackage(ReactViroPackage.ViroPlatform.valueOf("AR")));
-          
           return packages;
         }
 
--- src/private/components/Home.js
+++ src/private/components/Home.js
@@ -1,55 +1,16 @@
-import React, {useState} from 'react';
-import {StyleSheet} from 'react-native';
+import React from 'react';
+import { createDrawerNavigator } from '@react-navigation/drawer';
 import {
-  ViroARScene,
-  ViroText,
-  ViroTrackingStateConstants,
-  ViroARSceneNavigator,
-} from '@viro-community/react-viro';
+  Text,
+  View,
+} from 'native-base';
 
-const HelloWorldSceneAR = () => {
-  const [text, setText] = useState('Initializing AR...');
-
-  function onInitialized(state, reason) {
-    console.log('guncelleme', state, reason);
-    if (state === ViroTrackingStateConstants.TRACKING_NORMAL) {
-      setText('Crisleivys becerra');
-    } else if (state === ViroTrackingStateConstants.TRACKING_UNAVAILABLE) {
-      // Handle loss of tracking
-    }
-  }
-
+const Home = () => {
   return (
-    <ViroARScene onTrackingUpdated={onInitialized}>
-      <ViroText
-        text={text}
-        scale={[0.5, 0.5, 0.5]}
-        position={[0, 0, -1]}
-        style={styles.helloWorldTextStyle}
-      />
-    </ViroARScene>
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Home Screen Private</Text>
+    </View>
   );
 };
 
-export default () => {
-  return (
-    <ViroARSceneNavigator
-      autofocus={true}
-      initialScene={{
-        scene: HelloWorldSceneAR,
-      }}
-      style={styles.f1}
-    />
-  );
-};
-
-var styles = StyleSheet.create({
-  f1: {flex: 1},
-  helloWorldTextStyle: {
-    fontFamily: 'Arial',
-    fontSize: 30,
-    color: '#ffffff',
-    textAlignVertical: 'center',
-    textAlign: 'center',
-  },
-});
+export default Home;
