765670c5a17139a7b5efd63aab7f63fa4d30c6b6
Crisleivys Gil <crisleivysngil@gmail.com>
2023-05-03 12:38:55 -0400

    Cerrar sesion

--- src/private/components/Logout.js
+++ src/private/components/Logout.js
@@ -1,15 +1,48 @@
-import React from 'react'
+import React,{useState,useEffect} from 'react'
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
 import {
     Text,
     View,
+    Button,
+    Image
   } from 'native-base';
+  import style from '~styles';
 
-const Logout = () => {
+const Logout = ({navigation}) => {
+  const selected= require('./images/error.png')
+  const navigate = navigation.navigate;
+  const [isMounted, setIsMounted] = useState(true);
+
+  useEffect(() => {
+    return () => {
+      setIsMounted(false);
+    };
+  }, []);
+
+  const handleLogout = async () => {
+    await AsyncStorage.removeItem('token');
+    await AsyncStorage.removeItem('userData');
+
+    if (isMounted) {
+      navigate('PublicRouter', { screen: 'Login' });
+    }
+
+  };
   return (
-    <View> 
-      <Text>Logout</Text>
-    </View>
+    <View style={{ flex: 3, justifyContent:'center', paddingHorizontal: 20, width: '100%' }} id="alert-register">
+    <Image
+      justifyContent="center"
+      alignItems="center"
+      source={selected}
+      alt="image"
+      size="2xl"
+      key="lg"
+      resizeMode="cover"
+      mx="auto" />
+      <Text style={{color:style.color.primary, fontWeight: 'bold', fontSize:18, textAlign:'center', paddingVertical:20}} >¿Está seguro que desea cerrar sesión?</Text>
+      <Button style={{ ...style.button.primary }} _text={{ color: style.color.secondary }} onPress={handleLogout} >Cerrar sesión</Button>
+  </View>
   )
 }
 
